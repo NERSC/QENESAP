5355635a105e9d42c42d6f17c06e3fa13706fd6f
Hans Pabst <hans.pabst@intel.com>
2016-06-03 15:00:18 +0200

    Configure wrapper scripts for QE's configure script. The wrapper makes sure to pick up Intel SW Development Tools in an optimal fashion; the Intel Tools need to be source'd before (source /software/intel/compilers_and_libraries_2017.0.064/linux/bin/compilervars.sh intel64). One might change line #11 of the wrapper to adjust for a different ELPA installation directory; ELPA has been configured/built using the ELPA configure wrapper scripts committed previously. The wrappers have been tested with QE 5.3.0. Please note that these wrapper scripts are *not* tested and not yet working for QE 5.4.0!

---
+++ dev/intel.make/configure-qe-snb.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+TARGET="-xAVX"
+OMPFLAG="-qopenmp -qoverride_limits"
+#IPO="-ipo-separate"
+OPTC=-O3
+OPTF=-O2
+PRFX=2017-
+
+HERE=$(cd $(dirname $0); pwd -P)
+export ELPAROOT="${HERE}/../elpa/${PRFX}snb-mkl1"
+export MKLRTL="sequential"
+#export MKLRTL="intel_thread"
+#export OPENMP="--enable-openmp"
+export LD_LIBS="-Wl,--as-needed -liomp5 -Wl,--no-as-needed"
+export MPIF90=mpiifort
+export AR=xiar
+
+export BLAS_LIBS="-Wl,--start-group \
+    \$(MKLROOT)/lib/intel64/libmkl_intel_lp64.a \
+    \$(MKLROOT)/lib/intel64/libmkl_core.a \
+    \$(MKLROOT)/lib/intel64/libmkl_${MKLRTL}.a \
+    \$(MKLROOT)/lib/intel64/libmkl_blacs_intelmpi_lp64.a \
+  -Wl,--end-group"
+export LAPACK_LIBS="\$(BLAS_LIBS)"
+export SCALAPACK_LIBS="\$(MKLROOT)/lib/intel64/libmkl_scalapack_lp64.a"
+export FFT_LIBS="\$(BLAS_LIBS)"
+
+./configure ${OPENMP} --with-elpa=${ELPAROOT} --with-scalapack=intel $*
+
+# adjust generated configuration
+sed -i \
+  -e "s/-nomodule -openmp/-nomodule/" \
+  -e "s/-par-report0 -vec-report0//" \
+  -e "s/-D__FFTW3/-D__DFTI/" \
+  make.sys
+sed -i \
+  -e "s/-D__FFTW/-D__DFTI/" -e "s/-D__ELPA/-D__ELPA3/" \
+  -e "s/IFLAGS         = -I\.\.\/include/IFLAGS         = -I\.\.\/include -I\$(MKLROOT)\/include\/fftw/" \
+  -e "s/-O3/${OPTC} ${IPO} ${TARGET} -fno-alias -ansi-alias/" \
+  -e "s/-O2 -assume byterecl -g -traceback/${OPTF} -align array64byte ${IPO} ${TARGET} -assume byterecl/" \
+  -e "s/LDFLAGS        =/LDFLAGS        = -static-intel -static-libgcc -static-libstdc++/" \
+  -e "s/-openmp/${OMPFLAG}/" \
+  make.sys
+
+# Uncomment below block in case of compiler issue (ICE)
+echo >> make.sys
+cat configure-qe-tbbmalloc.mak >> make.sys
+echo -e "init_us_1.o: init_us_1.f90\n\t\$(MPIF90) \$(F90FLAGS) -O1 -c \$<\n" >> make.sys
+echo -e "new_ns.o: new_ns.f90\n\t\$(MPIF90) \$(F90FLAGS) -O1 -c \$<\n" >> make.sys
+echo -e "us_exx.o: us_exx.f90\n\t\$(MPIF90) \$(F90FLAGS) ${OMPFLAG} -c \$<\n" >> make.sys
+
+#sed -i -e "s/\$(MOD_FLAG)\.\.\/ELPA\/src/\$(MOD_FLAG)${ELPAROOT}\/src/" Modules/Makefile
+sed -i -e "s/\$(MOD_FLAG)\.\.\/ELPA\/src//" Modules/Makefile
+
+# patch source code files
+patch -N PW/src/setup.f90 configure-qe-setup_pw.patch
+patch -N Modules/dspev_drv.f90 configure-qe-dspev_drv.patch
+patch -N Modules/zhpev_drv.f90 configure-qe-zhpev_drv.patch
+#patch -N Modules/wavefunctions.f90 configure-qe-wavefunctions.patch
+#patch -N FFTXlib/fft_parallel.f90 configure-qe-fft_parallel.patch
+patch -N FFTXlib/fftw.c configure-qe-fftw.patch
+
+# reminder
+echo "Ready to \"make pw\"!"
+
